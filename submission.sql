52a82c29c2d2a91d6c79b10a408635bd3f5adf78
Aiden Graham <aiden0405@gmail.com>
2025-10-30 18:08:25 -0500

    submission 1

---
+++ submission.sql
@@ -0,0 +1,34 @@
+-- Discovered post: 
+-- POST AUTHOR USERNAME: smart-money-44
+-- POST ID: nbZY_
+-- POST TITLE: Get rich fast 
+-- POST CONTENT: You should all invest in EmptyStack Solutions soon or you'll regret it. My dad works there and he's got some serious inside intel. Their self-driving taxis are the future and the future is here.
+
+-- smart-money-44 
+-- NAME: Brad Steele
+
+--    username        |    password     | first_name | last_name
+----------------+-------------+------------+-----------
+-- triple-cart-38     | password456     | Andrew     | Steele
+-- lance-main-11      | password789     | Lance      | Steele
+-- worse-insolence-73 | SJXJhMrH2jqjBJ_ | Brad       | Smith
+
+-- Andrew Steele's forum username is 'sharp-engine-57' (Brad's dad?)
+
+-- Emptystack message about self-driving taxis:
+
+-- id: LidWj
+-- from: your-boss-99
+-- to: triple-cart-38
+-- subject: Project TAXI
+-- body: Deploy Project TAXI by end of week. We need this out ASAP.
+
+-- Emptystack project:
+-- id: DczE0v2b
+-- code: TAXI
+
+-- your-boss-99 emptystack account:
+-- Password: notagaincarter
+-- Name: Skylar Singer
+
+-- // SHUT DOWN THE PROJECT. PROGRESS WIPED. MISSION SUCCESS //
